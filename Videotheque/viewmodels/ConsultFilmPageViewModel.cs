3458952590d12ac14e40491b62ce895dae670ffa
Alexis Cabrol <alexiscabrololivaux@gmail.com>
2019-06-01 10:55:29 +0100

    Update UI in film consult

--- Videotheque/viewmodels/ConsultFilmPageViewModel.cs
+++ Videotheque/viewmodels/ConsultFilmPageViewModel.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Videotheque.config;
 using Videotheque.models;
+using Videotheque.views;
 
 namespace Videotheque.viewmodels
 {
@@ -25,6 +27,17 @@ namespace Videotheque.viewmodels
         {
             get { return GetValue<Film>(); }
             set { SetValue<Film>(value); }
+        }
+
+        public Command GoBack
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    SuperViewModel.Source = NavigationCache.GetPage<FilmPage, FilmPageViewModel>(SuperViewModel);
+                });
+            }
         }
     }
 }
